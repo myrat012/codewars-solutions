a62cd94c1ce832d3a9565e9af7e15c5c1c517895
Agamyrat Erlekov <erlekow1997@gmail.com>
2025-06-02 11:50:12 +0500

    add solution count_of_positives

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"codewars/easy"
+	"fmt"
+)
+
+func main() {
+	s := []string{"bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "Perhaps"}
+	fmt.Println(easy.TwoSort(s))
+}
